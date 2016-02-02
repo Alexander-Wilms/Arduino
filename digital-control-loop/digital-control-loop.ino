4a485ce980c5911dd081a4046215cf875ee01088
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-02-03 00:55:29 +0100

    Simple control loop

---
+++ digital-control-loop/digital-control-loop.ino
@@ -0,0 +1,45 @@
+int w = 0;
+int e = 0;
+int e_alt = 0;
+int y = 0;
+int y_alt = 0;
+int x = 0;
+int wPin = A0;
+int yPin = 3; // Pin muss PWM unterstützen
+int xPin = A1;
+int t_alt;
+int rechenzeit;
+int c1 = 5;
+int c2 = 3;
+
+void setup() {
+  pinMode(wPin, INPUT);
+  pinMode(xPin, INPUT);
+  pinMode(yPin, OUTPUT);
+}
+
+void loop() {
+  // aktuelle Zeit speichern
+  t_alt = millis();
+
+  // Sollgröße messen
+  w = analogRead(wPin);
+  
+  // Stellgröße messen
+  x = analogRead(xPin);
+
+  // Stellfehler berechnen
+  e_alt = e;
+  e = w - x;
+
+  // PI-Algorithmus, um Stellgröße zu berechnen
+  y_alt = y;
+  y = y_alt + c1*e - c2*e_alt;
+
+  // Stellgröße ausgeben
+  analogWrite(yPin, y);
+
+  // warten, bis TA vergangen ist
+  rechenzeit = millis() - t_alt;
+  delay(1000-rechenzeit);
+}
