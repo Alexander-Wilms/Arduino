5ea15f08a0fa0d48adcf9255b23042fb22870b3b
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-02-13 22:30:32 +0100

    Add documentation

--- ultrasonic-distance-sensor/ultrasonic-distance-sensor.ino
+++ ultrasonic-distance-sensor/ultrasonic-distance-sensor.ino
@@ -5,6 +5,8 @@
 #define Echo 2
 float distance;
 int pulselength;
+int t1, t2;
+int delta_t;
 
 void setup() {
   pinMode(Trig, OUTPUT);
@@ -13,16 +15,25 @@ void setup() {
 }
 
 void loop() {
+  t1 = millis();
+  
   digitalWrite(Trig, HIGH);
   delay(1);
   digitalWrite(Trig, LOW);
 
+  // measure pulse length in microseconds
   pulselength = pulseIn(Echo, HIGH);
-  // ms to cm
-  distance = pulselength / 29 / 2;
+  // microseconds to meters
+  distance = abs(pulselength) / 1000000. * 343.5 / 2.;
 
-  Serial.println(pulselength);
-  Serial.println(distance);
+  Serial.print(distance);
+  Serial.println(" m");
 
-  delay(1000);
+  t2 = millis();
+  delta_t = t2 - t1;
+  // Serial.print("delta_t: ");
+  // Serial.println(delta_t);
+
+  // delta_t is at most ca. 180
+  delay(200-delta_t);
 }
