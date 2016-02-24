b1bd2992da641dc1bf0c56e6c73c55d2eecdff93
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-02-24 12:48:22 +0100

    Fix bode plot

--- bode-plot/bode_plot.m
+++ bode-plot/bode_plot.m
@@ -5,8 +5,16 @@ C = 10^-6;
 f = [62500 62500 62500 62500 62500];
 x = [1 8 64 256 1024];
 a = f./x
-y = [520 534 633 885 1022];
-semilogx(a,y);
+w = a*2*3.14
+y = [517 533 637 886 1022];
+y = 20 * log (y./1022)
+
+s = tf('s');
+a = R * C
+sys = 1 / (1+a*s)
+bodemag(sys)
+hold on
+semilogx(w,y,'r');
 f_eck=1/(R*C); %Eckfrequenz
 hold on
 line([f_eck f_eck], get(gca,'YLim'))
